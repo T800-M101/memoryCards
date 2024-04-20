7e959c296d423e604d6238df48405f3039ceda30
Guillermo Moran <tuahil@gmail.com>
2024-04-20 14:50:27 -0600

    add start page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NewGameComponent } from './newgame/new-game.component';
+import { GameboardComponent } from './gameboard/gameboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: NewGameComponent
+    },
+    {
+        path:'game-board',
+        component:GameboardComponent 
+    }
+];
---
+++ src/app/newgame/new-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-game.component.html',
+  styleUrl: './new-game.component.css'
+})
+export class NewGameComponent {
+
+}
